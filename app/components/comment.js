1067edc268260d1c5474b0d64ecc8e455fad279f
jaeseonglee1008 <jaeseonglee@umass.edu>
2017-04-07 17:24:31 -0400

    fb8

--- app/components/comment.js
+++ app/components/comment.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import {unixTimeToString} from '../util';
 import {Link} from 'react-router';
+import {likeComment, unlikeComment} from '../server';
+
 export default class Comment extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { likeCounter: this.props.likeCounter };
+    }
+
+  handleLikeClick(clickEvent) {
+      clickEvent.preventDefault();
+
+      if (clickEvent.button === 0) {
+          var callbackFunction = (updatedLikeCounter) => {
+              this.setState({likeCounter: updatedLikeCounter});
+          };
+
+          if (this.didUserLike()) {
+              unlikeComment(this.props.feedItemId, this.props.index, 4, callbackFunction);
+          } else {
+              likeComment(this.props.feedItemId, this.props.index, 4, callbackFunction);
+          }
+      }
+  }
+
+    didUserLike() {
+        var likeCounter = this.state.likeCounter;
+        var liked = false;
+        // Look for a likeCounter entry with userId 4 -- which is the
+        // current user.
+        for (var i = 0; i < likeCounter.length; i++) {
+            if (likeCounter[i]._id === 4) {
+                liked = true;
+                break;
+            }
+        }
+        return liked;
+    }
+
   render() {
+      var likeButtonText = "Like";
+      if (this.didUserLike()) {
+          likeButtonText = "Unlike";
+      }
     return (
       <div>
         <div className="media-left media-top">
@@ -10,8 +51,8 @@ export default class Comment extends React.Component {
         </div>
         <div className="media-body">
           <Link to={"/profile/" + this.props.author._id}>{this.props.author.fullName}</Link> {this.props.children}
-          <br /><a href="#">Like</a> · <a href="#">Reply</a> ·
-          {unixTimeToString(this.props.postDate)}
+          <br /><a href="#" onClick={(e) => this.handleLikeClick(e)}>{this.state.likeCounter.length} <span className="glyphicon glyphicon-thumbs-up"></span> {likeButtonText}</a> · <a href="#">Reply</a> ·
+            {unixTimeToString(this.props.postDate)}
         </div>
       </div>
     )
--- app/components/feeditem.js
+++ app/components/feeditem.js
@@ -130,7 +130,7 @@ export default class FeedItem extends React.Component {
                     data.comments.map((comment, i) => {
                         // i is comment's index in comments array
                         return (
-                                <Comment key={i} author={comment.author} postDate={comment.postDate}>{comment.contents}</Comment>
+                          <Comment key={i} author={comment.author} postDate={comment.postDate} index={i} feedItemId={data._id} likeCounter={comment.likeCounter} >{comment.contents}</Comment>
                         );
                     })
                 }
